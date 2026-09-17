73ab18f58c4cf2eef79f380c8a7286db508410c9
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2026-09-17 22:57:59 +0100

    Update polling_unit URL pattern to include ID

--- polls/urls.py
+++ polls/urls.py
@@ -8,7 +8,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", polling_unit, name="polling_unit"),
+    path("polling-unit/<int:polling_unit_id>/", polling_unit, name="polling_unit"),
     path("sum_total", sum_polling_unit, name="sum_polling_unit"),
     path("add_new", add_new_polling_unit, name="add_new_polling_unit"),
 ]
