bb0f510aaaa627e6cb0ec7018a611457329728fc
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2026-09-18 00:16:07 +0100

    Update database settings to use SQLite

--- Bincom_project/settings.py
+++ Bincom_project/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'Bincom_project.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "bincomphptest",
-        "USER": "root",
-        "PASSWORD": "your_password",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
