e3b5da8b1be7254291f4b978439a067b3cd5fa28
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2026-09-17 23:28:31 +0100

    Add new route for creating polling unit

--- polls/urls.py
+++ polls/urls.py
@@ -10,4 +10,5 @@ from . import views
 urlpatterns = [
     path("polling-unit/<int:polling_unit_id>/", polling_unit, name="polling_unit"),
     path("lga-results/", views.lga_results, name="lga_results"),
+    path("polling-unit/new/", views.create_polling_unit, name="create_polling_unit"),
 ]
