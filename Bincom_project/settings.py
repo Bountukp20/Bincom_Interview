e4cfb908ac22f68703e59d8b5b3a74fecdb41432
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2026-09-17 22:47:30 +0100

    Refactor Django settings for consistency and clarity

--- Bincom_project/settings.py
+++ Bincom_project/settings.py
@@ -77,17 +77,13 @@ WSGI_APPLICATION = 'Bincom_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bincomphptest',
-        'USER': 'root',
-        'PASSWORD': 'Marvuk@2204',  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306',  
-        
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "bincomphptest",
+        "USER": "root",
+        "PASSWORD": "your_password",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
