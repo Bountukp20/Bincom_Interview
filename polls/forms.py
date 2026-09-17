42fccacfb507113273d7a89e734a207145db55ac
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2026-09-17 23:24:42 +0100

    Refactor PollingUnitForm to use ChoiceField
    Updated PollingUnitForm to use ChoiceField for lga_id and ward_id with dynamic choices.

--- polls/forms.py
+++ polls/forms.py
@@ -1,13 +1,11 @@
 class PollingUnitForm(forms.ModelForm):
 
-    lga_id = forms.ModelChoiceField(
-        queryset=LGA.objects.filter(state_id=25).order_by("lga_name"),
-        empty_label="Select LGA"
+    lga_id = forms.ChoiceField(
+        label="Local Government"
     )
 
-    ward_id = forms.ModelChoiceField(
-        queryset=Ward.objects.all().order_by("ward_name"),
-        empty_label="Select Ward"
+    ward_id = forms.ChoiceField(
+        label="Ward"
     )
 
     class Meta:
@@ -15,11 +13,31 @@ class PollingUnitForm(forms.ModelForm):
 
         fields = [
             "polling_unit_id",
-            "ward_id",
             "lga_id",
+            "ward_id",
             "polling_unit_number",
             "polling_unit_name",
             "polling_unit_description",
             "lat",
             "long",
+        ]
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.fields["lga_id"].choices = [
+            ("", "-- Select LGA --")
+        ] + [
+            (lga.lga_id, lga.lga_name)
+            for lga in LGA.objects.filter(
+                state_id=25
+            ).order_by("lga_name")
+        ]
+
+        self.fields["ward_id"].choices = [
+            ("", "-- Select Ward --")
+        ] + [
+            (ward.ward_id, ward.ward_name)
+            for ward in Ward.objects.all().order_by("ward_name")
         ]
